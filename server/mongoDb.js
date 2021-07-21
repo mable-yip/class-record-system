ffaa43f3864960e63bd1f40459017e80d98eb307
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-07-21 14:41:26 +1000

    inital commit

--- server/mongoDb.js
+++ server/mongoDb.js
@@ -1,16 +1,17 @@
 import { default as mongodb } from 'mongodb'
+import dotenv from "dotenv";
+dotenv.config({ silent: process.env.NODE_ENV === 'production' });
 
-const CONNECTION_URL = `mongodb+srv://hoypqn4:amLP5483@cluster0.6iqab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-const DATABASE_NAME = "class-record-system"
 
 let database;
 const initDb = () => {
-  mongodb.MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+  console.log(process.env.CONNECTION_URL)
+  mongodb.MongoClient.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
       throw error;
     }
-    database = client.db(DATABASE_NAME);
-    console.log("Connected to `" + DATABASE_NAME + "`!");
+    database = client.db(process.env.DATABASE_NAME);
+    console.log("Connected to `" + process.env.DATABASE_NAME + "`!");
   })
 }
 
